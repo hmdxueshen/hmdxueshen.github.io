17ab99cfc5a1b997b06123921fd9a4376a79999c
hmdxueshen <hmdphs@163.com>
2018-05-01 22:05:57 +0800

    Delete showPic.js

--- showPic.js
+++
@@ -1,9 +0,0 @@
-function showPic(whicp)
-{
-	var soure = 	whicp.getAttribute("href");
-	var pla = 	document.getElementById("zwf");
-	pla.setAttribute("src",soure);	
-	var text = whicp.getAttribute("title");
-	var des = document.getElementById("des");
-	des.firstChild.nodeValue = text;
-}
